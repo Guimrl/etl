ad8052cdcb65ec14b8503bfe901d0a6d145a5a5d
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-01-17 13:49:38 -0300

    lendo arquivos txt

--- index.php
+++ index.php
@@ -4,9 +4,17 @@ require __DIR__ . "/vendor/autoload.php";
 
 use src\Leitor;
 
-$leitor = new Leitor();
-$leitor->setDiretorio(__DIR__ . '/arquivos');
-$leitor->setArquivo('dados.csv');
-$leitor->lerArquivo();
+//***********TXT***********//
+$leitorTXT = new Leitor();
+$leitorTXT->setDiretorio(__DIR__ . '/arquivos');
+$leitorTXT->setArquivo('dados.txt');
+$arr_txt = $leitorTXT->lerArquivo();
 
-print_r($leitor->lerArquivo());
+//***********CSV***********//
+$leitorCSV = new leitor();
+$leitorCSV->setDiretorio(__DIR__ . '/arquivos');
+$leitorCSV->setArquivo('dados.csv');
+$arr_csv = $leitorCSV->lerArquivo();
+
+//***********TXT***********//
+print_r(array_merge($arr_txt, $arr_csv));
--- src/extrator/Arquivo.php
+++ src/extrator/Arquivo.php
@@ -10,7 +10,7 @@ class Arquivo
     {
         array_push($this->dados,
             [
-                'nome' => iconv('iso-8859-1', 'utf-8', $nome),
+                'nome' => iconv("iso-8859-1", "utf-8", $nome),
                 'cpf' => $cpf,
                 'email' => $email
             ]
@@ -30,5 +30,23 @@ class Arquivo
         while (($linha = fgetcsv($handle, 10000, ';')) !== false) {
             $this->setDados($linha[0], $linha[1], $linha[2]);
         }
+
+        fclose($handle);
+    }
+
+    public function lerArquivoTXT(string $caminho): void
+    {
+        $handle = fopen($caminho, 'r');
+
+        while (!feof($handle)) {
+            $linha = fgets($handle);
+            $this->setDados(
+                substr($linha, 11, 30),
+                substr($linha, 0, 11),
+                substr($linha, 41, 60)
+            );
+        }
+
+        fclose($handle);
     }
 }
