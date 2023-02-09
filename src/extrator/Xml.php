74bf455d361c9d8ec5a2cd4b82201ff11126fd52
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-02-09 15:47:39 -0300

    criando leitura de xml

---
+++ src/extrator/Xml.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace src\extrator;
+
+/**
+ * O formato de entrada de dados é: cpf, nome, email.
+ */
+class Xml extends Arquivo
+{
+    public function lerArquivo(string $caminho): array
+    {
+        $handle = fopen($caminho, 'r');
+
+        while (!feof($handle)) {
+            $linha = fgets($handle);
+            $this->setDados(
+                substr($linha, 11, 30),
+                substr($linha, 0, 11),
+                substr($linha, 41, 60)
+            );
+        }
+
+        fclose($handle);
+
+        return $this->getDados();
+    }
+}
